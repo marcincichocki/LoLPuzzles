6eab3eb535c328cf35af1aa8c19a1ea887ecb405
marcincichocki <marcincichocki1993@gmail.com>
2016-01-20 11:14:35 +0100

    STYLE: separate imports from component
    Imports are separated from game component by 2 blank lines, like in other
    files.

--- src/app/components/game/game.ts
+++ src/app/components/game/game.ts
@@ -5,6 +5,7 @@ import {SplashArtInfoComponent} from '../splashArtInfo/splashArtInfo';
 import {ForegroundComponent} from '../foreground/foreground';
 import {CanvasService} from '../../services/canvas/canvas.service';
 
+
 @Component({
   selector: 'game-component',
   template: require('./game.html'),
